033c6ab02b92671f9ecc7c487bfc1a68e31c9ce6
Jhonathan Meza <jmeza@nimbox.com>
2021-12-07 10:39:10 -0400

    Export class AxiosFetchError

--- dist/index.js
+++ dist/index.js
@@ -14,5 +14,54 @@ function nullify(s) {
     return t;
 }
 
-export { nullify };
+class FetchError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.status = null; // when code is 'response'
+        this.type = null; // when code is 'network'
+        this.code = code || 'error';
+    }
+}
+
+class AxiosFetchError extends FetchError {
+    constructor(error) {
+        super(error.message);
+        if (error.response) {
+            this.code = 'response';
+            this.status = error.response.status;
+            return;
+        }
+        if (error.request) {
+            this.code = 'network';
+            this.type = 'network';
+            return;
+        }
+    }
+}
+
+const placeholder = /(%%)|(%([sdj]))/g;
+const sprintf = (message, ...parameters) => {
+    let result = message ? message : '';
+    result = result.replace(placeholder, (match, escaped, pattern, flag) => {
+        if (escaped) {
+            return '%';
+        }
+        var p = parameters.shift();
+        switch (flag) {
+            case 's':
+                return String(p);
+            case 'd':
+                return Number(p).toString();
+            case 'j':
+                return JSON.stringify(p);
+        }
+        return '';
+    });
+    if (parameters.length > 0) {
+        result = result + ' ' + parameters.join(' ');
+    }
+    return result;
+};
+
+export { AxiosFetchError, FetchError, nullify, sprintf };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { nullify } from './nullify';
+export { AxiosFetchError } from './AxiosFetchError';
+export { FetchError } from './FetchError';
+export { FetchErrorCode } from './FetchErrorCode';
+export { sprintf } from './sprintf';
