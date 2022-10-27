d07692e338f0c6533e820e13076d1025caede747
Ricardo Marimón <rmarimon@nimbox.com>
2022-10-27 15:55:46 -0400

    Remove dist files

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { nullify } from './nullify';
-export { AxiosFetchError } from './AxiosFetchError';
-export { FetchError } from './FetchError';
-export { FetchErrorCode } from './FetchErrorCode';
-export { sprintf } from './sprintf';
