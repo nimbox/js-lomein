def197731e1d26a4bba187fbae594898f38c548e
Ricardo Marimón <rmarimon@nimbox.com>
2022-10-28 16:10:50 -0400

    Export to es2020

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import typescript from 'rollup-plugin-typescript2';
-import pack from './package.json' assert { type: "json" };
+
 
 export default {
 
@@ -17,17 +17,12 @@ export default {
         sourcemap: true
     }],
 
-    external: [
-        ...Object.keys(pack.dependencies || {}),
-        ...Object.keys(pack.peerDependencies || {})
-    ],
-
     plugins: [
 
         typescript({
             clean: true,
             tsconfigOverride: {
-                exclude: ["src/icons", "src/styles", "stories/**/*", "**/*.stories.tsx", "src/i18n.tsx"]
+                exclude: []
             }
         })
 
