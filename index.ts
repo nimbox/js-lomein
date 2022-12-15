648372eeb478992c5892420cf9b97246ad154df2
Jhonathan Meza <48058861+meza-jhonathan@users.noreply.github.com>
2022-12-15 14:50:46 -0400

    Export uuid

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ export { FetchErrorCode } from './src/FetchErrorCode';
 export { sprintf } from './src/sprintf';
 export { token } from './src/token';
 export { nullify } from './src/nullify';
+export { uuid } from './src/uuid';
